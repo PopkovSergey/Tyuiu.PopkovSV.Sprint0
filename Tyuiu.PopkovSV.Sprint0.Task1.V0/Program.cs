ac47e2d2c5b686581f95e3af7f4e0a138a62d460
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-09-14 18:55:18 +0300

    Task1

---
+++ Tyuiu.PopkovSV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PopkovSV.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Сергей";
+            int age = 17;
+            Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.ReadKey();
+        }
+    }
+}
