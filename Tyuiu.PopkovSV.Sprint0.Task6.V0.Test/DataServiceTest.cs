15384b2e16f4691664bf17aa98dc563ad3d5486e
Tyuiu.PopkovSV <popkovsergej10@gmail.com>
2023-09-17 15:22:54 +0300

    Task6

---
+++ Tyuiu.PopkovSV.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.PopkovSV.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.PopkovSV.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckSubtractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        [TestMethod]
+        public void CheckMultArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
---
+++ Tyuiu.PopkovSV.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PopkovSV.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.PopkovSV.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            //Пример циклической структуры находится в библиотеке классов в методе AdditionArray
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numsArray));
+
+            //Пример циклической структуры находится в библиотеке классов в методе SubtractionArray
+            Console.WriteLine("Разность элементов массива = " + DataService.SubtractionArray(numsArray));
+
+            //Пример циклической структуры находится в библиотеке классов в методе MultiplicationArray
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
